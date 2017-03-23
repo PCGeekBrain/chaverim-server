8b473c0d3abd4b685f0d80d8ff484722fd749d65
PCGeek <mendelh1537@gmail.com>
2017-03-23 00:08:19 -0400

    Logs shown

---
+++ public/app/controllers/log.js
@@ -0,0 +1,22 @@
+angular.module('routerApp')
+.controller('LogsController', function($scope, $http, store, $state, $mdDialog, httpCall){
+    var vm = this;
+    $scope.log = []
+    $scope.sortType     = '_id'; // set the default sort type
+    $scope.sortReverse  = false;  // set the default sort order
+    $scope.searchAccount   = '';     // set the default search/filter term
+
+    $scope.refresh = function(){
+        var request = {method: 'GET',   url: '/api/calls/all',
+            headers: {
+                'Authorization': store.get('jwt'),
+            }}
+        var resultcb = function(res){
+            if(res.data.success){$scope.log = res.data.calls;}
+        }
+        httpCall.execute(request, resultcb);
+    }
+
+    $scope.refresh();
+
+});
