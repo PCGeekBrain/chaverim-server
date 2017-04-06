47ccb4aae22f2b4f26032d156fb207c5c65b0580
Mendel Hornbacher <Mendel Hornbacher>
2017-04-05 21:05:13 -0400

    Updates to the deployment

--- git-dev-master.cmd
+++ git-dev-master.cmd
@@ -1,6 +1,22 @@
+@echo off
+
+echo
+echo Pushing development branch to GitHub
 git push
+
+echo
+echo ========Checking out master=========
 git checkout master
+echo
+echo =============Merging================
 git merge development
+echo
+echo =============Pushing================
 git push
+echo
+echo ========Deploying on Server=========
+bash -c "ssh -t dh_chavarim@ps565483.dreamhost.com './deploy.sh'"
+
+echo
+echo ========Checking out development===
 git checkout development
-bash -c "ssh -t dh_chavarim@ps565483.dreamhost.com -pw iWTinAk9 './deploy.sh'"
